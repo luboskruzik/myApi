64b3445e3d3eac8d5a72cde1ffa140f19667d9dc
Lubos Kruzik <luboskruzik@gmail.com>
2021-01-29 15:52:01 +0100

    complete api endpoints

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,24 +8,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
-    private $passwordEncoder;
-
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $this->passwordEncoder = $passwordEncoder;
-    }
-
     /**
      * @param Request $request
      * @return Response
-     * @Route ("/api/user",  methods={"POST"})
+     * @Route ("/api/user", methods={"POST"})
      */
-    public function saveUser(Request $request): Response
+    public function saveUser(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $jsonContent = $request->getContent();
         $content = json_decode($jsonContent);
@@ -33,7 +27,7 @@ class ApiController extends AbstractController
         $user = new User();
         $user->setEmail($content->email);
         $user->setRoles($content->roles);
-        $user->setPassword($this->passwordEncoder->encodePassword(
+        $user->setPassword($passwordEncoder->encodePassword(
             $user,
             $content->password
         ));
@@ -44,16 +38,56 @@ class ApiController extends AbstractController
 
         return new Response(
             $jsonContent,
-            Response::HTTP_OK,
+            Response::HTTP_CREATED,
             [
                 'Content-Type' => 'application-json',
                 'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
             ]
         );
     }
+
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route ("/api/user", methods={"PUT"})
+     */
+    public function updateUser(Request $request, UserPasswordEncoderInterface $passwordEncoder, UserRepository $userRepository)
+    {
+        $jsonContent = $request->getContent();
+        $content = json_decode($jsonContent);
+
+        $user = $userRepository->find($content->id);
+
+        if (isset($content->email)) {
+            $user->setEmail($content->email);
+        }
+        if (isset($content->roles)) {
+            $user->setRoles($content->roles);
+        }
+        if (isset($content->password)) {
+            $user->setPassword($passwordEncoder->encodePassword(
+                $user,
+                $content->password
+            ));
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+
+        return new Response(
+            $jsonContent,
+            Response::HTTP_CREATED,
+            [
+                'Content-Type' => 'application-json',
+                'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
+            ]
+        );
+    }
+
     /**
      * @return Response
-     * @Route ("/api/user",  methods={"OPTIONS"})
+     * @Route ("/api/user/{id}", methods={"OPTIONS"})
      */
     public function preflight()
     {
@@ -63,19 +97,21 @@ class ApiController extends AbstractController
             [
                 'Content-Type' => 'application-json',
                 'Access-Control-Allow-Headers' => 'Content-Type',
-                'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
+                'Access-Control-Allow-Origin' => 'https://editor.swagger.io',
+                'Access-Control-Allow-Methods' => 'PUT, DELETE'
             ]
         );
     }
 
     /**
+     * @param UserRepository $user
+     * @param SerializerInterface $serializer
      * @return Response
-     * @Route ("/api/users",  methods={"GET"})
+     * @Route ("/api/users", methods={"GET"})
      */
-    public function getUsers(SerializerInterface $serializer)
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer)
     {
-        $user = $this->getDoctrine()->getRepository(User::class);
-        $users = $user->findAll();
+        $users = $userRepository->findAll();
 
         $jsonUsers = $serializer->serialize($users, 'json');
 
@@ -84,9 +120,52 @@ class ApiController extends AbstractController
             Response::HTTP_OK,
             [
                 'Content-Type' => 'application-json',
-                'Access-Control-Allow-Origin' => 'http://localhost:3355'
+                'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
             ]
         );
+    }
+
+    /**
+     * @return Response
+     * @Route ("/api/user/{id}", methods={"GET"})
+     */
+    public function getOneUser(UserRepository $userRepository,  SerializerInterface $serializer, $id)
+    {
+        $user = $userRepository->find($id);
+        $jsonUser = $serializer->serialize($user, 'json');
+
+        return new Response(
+            $jsonUser,
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application-json',
+                'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
+            ]
+        );
+    }
+
+    /**
+     * @Route ("/api/user/{id}", methods={"DELETE"})
+     *
+     */
+    public function deleteOneUser(UserRepository $userRepository, SerializerInterface $serializer, $id)
+    {
+        $user = $userRepository->find($id);
+        $jsonUser = $serializer->serialize($user, 'json');
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new Response(
+            $jsonUser,
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application-json',
+                'Access-Control-Allow-Origin' => 'https://editor.swagger.io'
+            ]
+        );
+
     }
 
 }
